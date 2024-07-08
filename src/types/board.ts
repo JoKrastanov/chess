65145bbdf6b907bd0597978636f87da579552fe9
Joan <jokrastanov@gmail.com>
2024-07-08 22:35:06 +0200

    add piece movement

--- src/types/board.ts
+++ src/types/board.ts
@@ -5,11 +5,13 @@ export class Board {
   boardDOM: Element;
   squaresDOM: NodeListOf<Element>;
   squares: number[];
+  turn: number; // 0: white, 1: black
 
   constructor() {
     this.squares = Array(64).fill(PieceCode.Empty);
     this.boardDOM = document.querySelector(".board") || document.createElement("div")
     this.squaresDOM = document.querySelectorAll(".square")
+    this.turn = 0
   }
 
   addPiece(squareIdx: number, piece: PieceCode) {
@@ -17,19 +19,15 @@ export class Board {
     const pieceDOM = document.createElement("span")
     this.squares[squareIdx] = piece
 
-    const boardDOMBounds = this.boardDOM.getBoundingClientRect()
-    const squareDOMBounds = squareDOM.getBoundingClientRect()
-
-    const offsetX = squareDOMBounds.left - boardDOMBounds.left;
-    const offsetY = squareDOMBounds.top - boardDOMBounds.top;
-
     pieceDOM.className = "piece"
+    pieceDOM.id = squareIdx.toString()
     pieceDOM.innerHTML = PieceIcon[piece]
 
-    pieceDOM.style.left = `${offsetX}px`;
-    pieceDOM.style.top = `${offsetY}px`;
-
-    this.boardDOM.appendChild(pieceDOM)
+    squareDOM.appendChild(pieceDOM)
     addPieceEventListeners(this.boardDOM, pieceDOM)
+  }
+
+  movePiece(squareOrigin: number) {
+
   }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,27 +1,9 @@
 export function addPieceEventListeners(boardDOM: Element, pieceDOM: HTMLSpanElement) {
+    pieceDOM.draggable = true;
+    pieceDOM.ondragstart = drag;
     const boardDOMBounds = boardDOM.getBoundingClientRect()
-    pieceDOM.addEventListener('mousedown', (e: MouseEvent) => {
-        const shiftX = e.clientX - pieceDOM.getBoundingClientRect().left + boardDOMBounds.left;
-        const shiftY = e.clientY - pieceDOM.getBoundingClientRect().top + boardDOMBounds.top;
+}
 
-        const moveAt = (pageX: number, pageY: number) => {
-            pieceDOM.style.left = pageX - shiftX + 'px';
-            pieceDOM.style.top = pageY - shiftY + 'px';
-        };
-
-        const onMouseMove = (e: MouseEvent) => {
-            moveAt(e.pageX, e.pageY);
-        };
-
-        document.addEventListener('mousemove', onMouseMove);
-
-        pieceDOM.addEventListener('mouseup', () => {
-            document.removeEventListener('mousemove', onMouseMove);
-            pieceDOM.onmouseup = null;
-        });
-
-        pieceDOM.ondragstart = () => {
-            return false;
-        };
-    });
+function drag(e: any) {
+    e.dataTransfer.setData("text", e.target.id);
 }
