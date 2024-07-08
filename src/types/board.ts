bf9e600821696fd86be9ccdf4d9d2b545e23729b
Joan <jokrastanov@gmail.com>
2024-07-08 22:50:43 +0200

    render squares in board class + prevent piece from capturing itself

--- src/types/board.ts
+++ src/types/board.ts
@@ -1,4 +1,4 @@
-import { addPieceEventListeners } from "../utils";
+import { addPieceEventListeners, allowDrop, drop } from "../utils";
 import { PieceCode, PieceIcon } from "./piece";
 
 export class Board {
@@ -10,8 +10,24 @@ export class Board {
   constructor() {
     this.squares = Array(64).fill(PieceCode.Empty);
     this.boardDOM = document.querySelector(".board") || document.createElement("div")
+    this.drawSquares()
     this.squaresDOM = document.querySelectorAll(".square")
     this.turn = 0
+  }
+
+  drawSquares() {
+    for (let rank = 0; rank < 8; rank++) {
+      const rankDiv = document.createElement("div");
+      rankDiv.className = "rank"
+      this.boardDOM.appendChild(rankDiv)
+      for (let file = 0; file < 8; file++) {
+        const squareDiv = document.createElement("div")
+        squareDiv.className = "square"
+        squareDiv.ondrop = drop
+        squareDiv.ondragover = allowDrop
+        rankDiv.appendChild(squareDiv)
+      }
+    }
   }
 
   addPiece(squareIdx: number, piece: PieceCode) {
--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,28 @@
 export function addPieceEventListeners(boardDOM: Element, pieceDOM: HTMLSpanElement) {
     pieceDOM.draggable = true;
     pieceDOM.ondragstart = drag;
-    const boardDOMBounds = boardDOM.getBoundingClientRect()
 }
 
 function drag(e: any) {
     e.dataTransfer.setData("text", e.target.id);
+}
+
+export function drop(ev: any) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    if (document.getElementById(data) == ev.target) {
+        return
+    }
+    if (ev.target.innerHTML !== "") {
+        ev.target.innerHTML = ""
+        new Audio('./assets/audio/capture.mp3').play();
+    }
+    else {
+        new Audio('./assets/audio/move-self.mp3').play();
+    }
+    ev.target.appendChild(document.getElementById(data));
+}
+
+export function allowDrop(ev: any) {
+    ev.preventDefault();
 }
