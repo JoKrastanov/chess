3fe8fdab978da5f2e07ebe25a80d4eccb47a07f7
Joan Krastanov <jokrastanov@gmail.com>
2024-12-08 19:07:12 +0100

    create bitboard enums for later development

---
+++ src/types/bitboards.ts
@@ -0,0 +1,13 @@
+export interface BitBoard {
+    whitePawns: BigInt
+    whikeKnights: BigInt;
+    whiteRooks: BigInt;
+    whiteQueens: BigInt;
+    whiteKing: BigInt;
+
+    blackPawns: BigInt
+    blackKnights: BigInt;
+    blackRooks: BigInt;
+    blackQueens: BigInt;
+    blackKing: BigInt;
+}
